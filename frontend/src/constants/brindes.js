17ec7c21ccfe6b9a846c564c316951f07071d6c9
Miirog <igormdalcol@gmail.com>
2025-11-12 09:07:52 -0300

    texts

--- frontend/src/constants/brindes.js
+++ frontend/src/constants/brindes.js
@@ -1,9 +1,9 @@
 export const BRINDES_INFO = {
-  1: { nome: 'Brinde 1', descricao: 'Primeira conquista!', cor: '#FF007F', pontosRequisito: 300 },
-  2: { nome: 'Brinde 2', descricao: 'Seguindo firme!', cor: '#39FF14', pontosRequisito: 500 },
-  3: { nome: 'Brinde 3', descricao: 'Meio do caminho!', cor: '#3D026D', pontosRequisito: 800 },
-  4: { nome: 'Brinde 4', descricao: 'Quase lá!', cor: '#FF007F', pontosRequisito: 1000 },
-  5: { nome: 'Brinde 5', descricao: 'Excelência alcançada!', cor: '#1E002B', pontosRequisito: 1000 }
+  1: { nome: 'Brinde 1', descricao: 'chaveiro NFC!', cor: '#FF007F', pontosRequisito: 300 },
+  2: { nome: 'Brinde 2', descricao: 'Cordão!', cor: '#39FF14', pontosRequisito: 500 },
+  3: { nome: 'Brinde 3', descricao: 'berlock daZain!', cor: '#3D026D', pontosRequisito: 800 },
+  4: { nome: 'Brinde 4', descricao: 'berlock Oculos!', cor: '#FF007F', pontosRequisito: 1000 },
+  5: { nome: 'Brinde 5', descricao: 'Funko com a sua cara!', cor: '#1E002B', pontosRequisito: 1000 }
 }
 
 export const BRINDE_IDS = Object.keys(BRINDES_INFO).map(Number)
